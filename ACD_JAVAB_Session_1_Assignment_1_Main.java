f7cc955884d5521c7e07e5765c8d0b974d1f87cc
krushnadeb <krushna_deb@rediffmail.com>
2016-04-16 19:27:02 +0530

    Added files via upload

---
+++ ACD_JAVAB_Session_1_Assignment_1_Main.java
@@ -0,0 +1,12 @@
+//Write a program to print the sum of two numbers. One number should be int and another should be
+//float. 
+//The sum of two numbers should be in integer.
+public class ACD_JAVAB_Session_1_Assignment_1_Main {
+public static void main(String[] args) {	
+	int num1;float floatnum2;
+	num1=120;floatnum2=(float) 34.23;
+	
+	System.out.println((int)(num1+floatnum2));			
+
+	}
+}
